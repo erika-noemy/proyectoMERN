3a10a4907d77ec2a92a4c3f3ce9c294ed1c5a839
Erika <erikanoemychuya@gmail.com>
2022-01-24 13:28:15 -0400

    dar valor default

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import HolaMundo from './componentes/HolaMundo'; //el primer HolaMundo no deber ser igual que el segundo pero es recomendable que si
-import AdiosMundo from './componentes/AdiosMundo';
 import Saludar from './componentes/Saludar';
 
 function App() {
   const user = {
-    nombre: "akire",
+    nombre: "erika",
     edad: 26,
     color: "lila"
   };
--- src/componentes/Saludar.js
+++ src/componentes/Saludar.js
@@ -2,7 +2,7 @@ import React from "react";
 export default function Saludar (props){
    
     const { userInfo, saludarFn} = props; //asignacion por destructuring
-    const {nombre} =userInfo
+    const {nombre= "Anonimo"} =userInfo
     console.log(userInfo);
     return(
         <div>
